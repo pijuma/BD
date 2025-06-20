c9617f93e6e6154c23edb692ff5b976c58836c9b
Pietra <73311085+pijuma@users.noreply.github.com>
2025-06-20 01:49:49 -0300

    Update views.sql

--- views.sql
+++ views.sql
@@ -7,3 +7,14 @@ SELECT A.nome, A.sobrenome, A.telefone, D.codigo_disc, COUNT(*) AS Vezes_cursada
 	JOIN Disciplina AS D using(codigo_disc)
 	GROUP BY (A.nome, A.sobrenome, A.telefone, D.codigo_disc)
 	HAVING COUNT(*) > 1;
+
+--listar todos alunos que foram aprovados e em cada matéria que eles foram aprovados 
+CREATE VIEW aprovados AS
+SELECT M.nome_aluno, D.codigo_disc, AVG(N.nota) AS media
+	FROM Matriculas AS M
+	JOIN Notas_Matricula AS N USING(nome_aluno, sobrenome_aluno, telefone_aluno)
+	JOIN Turma AS T ON M.id_turma = T.id_turma
+	JOIN Disciplina AS D ON T.codigo_disc = D.codigo_disc
+	
+	GROUP BY M.nome_aluno, D.codigo_disc
+	HAVING AVG(N.nota) >= 5;
