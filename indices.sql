67392d0494cf723c9bc10e1e8f9d35f1064fdd86
Pietra <73311085+pijuma@users.noreply.github.com>
2025-06-22 22:26:07 -0300

    Update indices.sql

--- indices.sql
+++ indices.sql
@@ -1,3 +1,3 @@
-CREATE INDEX periodo_letivo ON Turma (semestre, ano);
-CREATE INDEX disc_turma ON Turma (codigo_disc);
-CREATE INDEX turma_mat ON Matriculas (id_turma);
+CREATE INDEX periodo_letivo ON Turma USING btree (semestre, ano);
+CREATE INDEX disc_turma ON Turma USING btree (codigo_disc);
+CREATE INDEX turma_mat ON Matriculas USING btree (id_turma);
